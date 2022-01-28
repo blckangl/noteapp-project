04ff5157b6febbf4938ab813838299e9fb08e8e8
Diakahril <dhia.kahri@lobsterware.com>
2022-01-28 19:50:55 +0100

    updating ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotesInputComponent } from './notes-input/notes-input.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { NoteElementComponent } from './note-element/note-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    SideNavComponent,
+    HomePageComponent,
+    NotesInputComponent,
+    NotesListComponent,
+    NoteElementComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/notes-input/notes-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-input',
+  templateUrl: './notes-input.component.html',
+  styleUrls: ['./notes-input.component.scss']
+})
+export class NotesInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
